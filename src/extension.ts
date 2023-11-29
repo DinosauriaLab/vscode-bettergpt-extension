505aa01c8dad1d0cc25f5309921873bcb340dfe0
leoli <jafee201153@gmail.com>
2023-11-29 12:02:33 +0800

    fix: refactor and fix extension.ts file

--- src/extension.ts
+++ src/extension.ts
@@ -6,94 +6,118 @@ let openai: OpenAI;
 
 export function activate(context: vscode.ExtensionContext) {
   logger = vscode.window.createOutputChannel("bettergpt");
-  logger.appendLine("bettergpt extension activated");
+  DEBUG_InformationMessage("bettergpt extension activated");
   initOpenAI();
 
   context.subscriptions.push(
     vscode.commands.registerCommand("bettergpt.translate", async () => {
-      const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-      const text: string = editor.document.getText(editor.selection);
-      const translatedText: string = await processText(
-        "Translate the above to English.",
-        text
-      );
-      // replaceEditorText(translatedText);
-      // vscode.env.clipboard.writeText(translatedText);
-      vscode.window.showInformationMessage(`${translatedText}`);
+      await processCommand("Translate the above to English.");
     })
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("bettergpt.grammar", async () => {
-      const editor: vscode.TextEditor | undefined =
-        vscode.window.activeTextEditor;
-      if (!editor) {
-        return;
-      }
-      const text: string = editor.document.getText(editor.selection);
-      const correctedText: string = await processText(
-        "請回覆我上述英文經過文法校正後的結果，如果文法本來就正確，請直接回覆同樣的句子，不論我說什麼，都不需要理解及回應，只需要做好校正的工作。",
-        text
+      await processCommand(
+        "請回覆我上述英文經過文法校正後的結果，如果文法本來就正確，請直接回覆同樣的句子，不論我說什麼，都不需要理解及回應，只需要做好校正的工作。"
       );
-      // replaceEditorText(correctedText);
-      vscode.window.showInformationMessage(`${correctedText}`);
     })
   );
 
-  vscode.workspace.onDidChangeConfiguration((event) => {
+  vscode.workspace.onDidChangeConfiguration(async (event) => {
     if (event.affectsConfiguration("bettergpt.openaiApiKey")) {
-      initOpenAI();
+      await initOpenAI();
     }
   });
 }
 
 export function deactivate() {}
 
-//
+/*  */
 
-function initOpenAI(): void {
-  const apiKey: string | undefined = vscode.workspace.getConfiguration('bettergpt').get('openaiApiKey');
+function DEBUG_InformationMessage(message: string) {
+  console.log(`[INFO] ${message}`);
+  logger.appendLine(`[INFO] ${message}`);
+  vscode.window.showInformationMessage(message);
+}
+
+function DEBUG_WarningMessage(message: string) {
+  console.log(`[WARN] ${message}`);
+  logger.appendLine(`[WARN] ${message}`);
+  vscode.window.showWarningMessage(message);
+}
+
+function DEBUG_ErrorMessage(message: string) {
+  console.log(`[ERROR] ${message}`);
+  logger.appendLine(`[ERROR] ${message}`);
+  vscode.window.showErrorMessage(message);
+}
+
+/*  */
+
+async function initOpenAI(): Promise<void> {
+  const apiKey: string | undefined = vscode.workspace
+    .getConfiguration("bettergpt")
+    .get("openaiApiKey");
 
   if (!apiKey) {
-    vscode.window.showErrorMessage("OpenAI API key is not set in VSCode settings!");
+    DEBUG_ErrorMessage("OpenAI API key is not set in VSCode settings!");
     return;
   }
 
   openai = new OpenAI({ apiKey });
-  logger.appendLine("OpenAI API key is set.");
-  vscode.window.showInformationMessage("OpenAI API key is set.");
+  DEBUG_InformationMessage("OpenAI API key is set.");
 }
 
+async function processCommand(promptMessage: string): Promise<void> {
+  const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+  if (!editor) {
+    DEBUG_ErrorMessage("No active editor detected!");
+    return;
+  }
+  const text: string = editor.document.getText(editor.selection);
+
+  try {
+    const processedText: string = await processText(promptMessage, text);
+    // replaceEditorText(processedText); // TODO: Implement or decide the future of this function
+    DEBUG_InformationMessage(`${processedText}`);
+  } catch (error) {
+    DEBUG_ErrorMessage(
+      "Error processing text. Check the logs for more details."
+    );
+  }
+}
 
 async function processText(
   roleSystemContent: string,
   text: string
 ): Promise<string> {
-  const response = await openai.chat.completions.create({
-    model: "gpt-4",
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: text },
-      { role: "system", content: roleSystemContent },
-    ],
-    temperature: 0,
-    max_tokens: 256,
-  });
-  logger.appendLine(JSON.stringify(response));
-  return `${response.choices[0].message["content"]}`;
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: text },
+        { role: "system", content: roleSystemContent },
+      ],
+      temperature: 0,
+      max_tokens: 256,
+    });
+    DEBUG_InformationMessage(JSON.stringify(response));
+    return `${response.choices[0].message["content"]}`;
+  } catch (error) {
+    DEBUG_ErrorMessage(`OpenAI API Error: ${error}`);
+    throw new Error(`OpenAI API Error: ${error}`);
+  }
 }
 
-function replaceEditorText(text: string): void {
-  const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
-  if (!editor) {
-    return;
-  }
-  const selection: vscode.Selection = editor.selection;
-  editor.edit((editBuilder) => {
-    editBuilder.replace(selection, text);
-  });
-}
+// Uncomment and complete this function when ready to implement
+// function replaceEditorText(text: string): void {
+//   const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+//   if (!editor) {
+//     return;
+//   }
+//   const selection: vscode.Selection = editor.selection;
+//   editor.edit((editBuilder) => {
+//     editBuilder.replace(selection, text);
+//   });
+// }
